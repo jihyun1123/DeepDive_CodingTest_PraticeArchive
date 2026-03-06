0e6481070e7140821e26e71f088ab850402c5fb4
jihyun1123 <janet9182@naver.com>
2026-03-06 16:42:27 +0900

    Remove 260305_06 folder

--- 260305_06/260224/BaekjoonCodingTest/Backjoon_반복문/test10.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync('input10.txt').toString().trim().split(' ');
-
-const num = Number(input[0]);
-
-for(let i = 1; i <= num; i++){
-  console.log(' '.repeat(num - i) + '*'.repeat(i));
-}
